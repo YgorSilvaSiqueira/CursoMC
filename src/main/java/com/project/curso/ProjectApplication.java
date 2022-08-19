b1aa04aaa3437d50a7e5bb5ce63b8bc11a1432d5
Ygor Siqueira <ygor.siqueira@nvoip.com.br>
2022-08-18 21:23:50 -0300

    Estado e cidade

--- src/main/java/com/project/curso/ProjectApplication.java
+++ src/main/java/com/project/curso/ProjectApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.project.curso.Respositories.CategoriaRepository;
+import com.project.curso.Respositories.CidadeRepository;
+import com.project.curso.Respositories.EstadoRepository;
 import com.project.curso.Respositories.ProdutoRepository;
 import com.project.curso.domain.Categoria;
+import com.project.curso.domain.Cidade;
+import com.project.curso.domain.Estado;
 import com.project.curso.domain.Produto;
 
 @SpringBootApplication
@@ -20,6 +24,12 @@ public class ProjectApplication implements CommandLineRunner {
 
 	@Autowired
 	private ProdutoRepository produtoRepository;
+
+	@Autowired
+	private EstadoRepository estadoRepository;
+
+	@Autowired
+	private CidadeRepository cidadeRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectApplication.class, args);
@@ -50,6 +60,24 @@ public class ProjectApplication implements CommandLineRunner {
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+
+		Estado est1 = new Estado(null, "Minas Gerais");
+
+		Estado est2 = new Estado(null, "São Paulo");
+
+		Cidade c1 = new Cidade(null, "Uberlandia", est1);
+
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+
+		est1.getCidades().addAll(Arrays.asList(c1));
+
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 
 	}
 
