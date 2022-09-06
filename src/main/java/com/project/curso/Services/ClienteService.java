af2e5f1cec8b4ea5940796d656f3c3c4d0fe3483
Ygor Siqueira <ygor.siqueira@nvoip.com.br>
2022-09-05 21:45:15 -0300

    endpoint /clientes/{id}

---
+++ src/main/java/com/project/curso/Services/ClienteService.java
@@ -0,0 +1,23 @@
+package com.project.curso.Services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.project.curso.Respositories.ClienteRepository;
+import com.project.curso.Services.Exceptions.ObjectNotFoundException;
+import com.project.curso.domain.Cliente;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+
+	public Cliente buscar(Integer id) {
+		Cliente obj = repo.findById(id).orElseThrow(() -> new ObjectNotFoundException(
+				"Cliente não encontrado! Id: " + id + " Tipo: " + Cliente.class.getName()));
+
+		return obj;
+	}
+
+}
