78b63bd3703b2936599453ca9001942316da0ade
Ygor Siqueira <ygor.siqueira@nvoip.com.br>
2022-10-21 11:31:46 -0300

    Alterando @JsonBackReference para @JsonIgnore e removendo @JsonManagedReference

--- src/main/java/com/project/curso/domain/Categoria.java
+++ src/main/java/com/project/curso/domain/Categoria.java
@@ -25,7 +25,6 @@ public class Categoria implements Serializable {
 	@Column(name = "name")
 	private String nome;
 
-	@JsonManagedReference
 	@ManyToMany(mappedBy = "categorias")
 	private List<Produto> produtos = new ArrayList<>();
 
